d29ffca19ae0eb1d56acfbb80dc891a33a156209
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-23 23:57:28 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -49,7 +49,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"'
-  Movie.all.find(["Movie_0",1, 2000,"Me"])
+  Movie.all.find(1)
   
 end
 
