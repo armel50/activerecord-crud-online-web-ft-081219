faad54df2ec31b3212a39f9b4e8f68f58c0c2aa1
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-24 00:04:38 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -54,7 +54,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-found Movie.all.detect{|el| el.title if el.release_date > 2000}
+found= Movie.all.detect{|el| el.title if el.release_date > 2000}
 end
 
 def can_be_found_updated_and_saved
